5731b430e4f819a58819954f950f0b6976ec8147
chris <mr.chrisnash@gmail.com>
2021-12-30 12:03:00 -0800

    password is now salted/hashed with bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,59 +1,62 @@
 //var async = require("async");
 var User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const { body, validationResult } = require("express-validator");
 
 // Display user create form on GET.
 exports.user_create_get = function (req, res, next) {
-    res.render('sign-up', { title: 'Create an new new account', error: false });
-  };
+  res.render('sign-up', { title: 'Create an new new account', error: false });
+};
 
 // Handle user create on POST.
 exports.user_create_post = [
-    // Validate and sanitize fields.
-    body("username")
-      .trim()
-      .isLength({ min: 3, max:15 })
-      .escape()
-      .withMessage("Username must be between 3-15 characters long."),
-    body("password")
-      .trim()
-      .isLength({ min: 6 })
-      .escape()
-      .withMessage("Password must be at least 6 characters long."),
-  
-    // Process request after validation and sanitization.
-    (req, res, next) => {
-      // Extract the validation errors from a request.
-      const errors = validationResult(req);
-  
-      if (!errors.isEmpty()) {
-        // There are errors. Render form again with sanitized values/errors messages.
-        console.log(errors);
-        res.render("sign-up", {
-          title: "Create a new account",
-          user: req.body,
-          errors: errors.array(),
-        });
-        return;
-      } else {
-        // Data from form is valid.
-  
-        // Create a user object with escaped and trimmed data.
-        var user = new User({
+  // Validate and sanitize fields.
+  body("username")
+    .trim()
+    .isLength({ min: 3, max: 15 })
+    .escape()
+    .withMessage("Username must be between 3-15 characters long."),
+  body("password")
+    .trim()
+    .isLength({ min: 6 })
+    .escape()
+    .withMessage("Password must be at least 6 characters long."),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      console.log(errors);
+      res.render("sign-up", {
+        title: "Create a new account",
+        user: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // // Data from form is valid.
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        // if err, do something
+        if (err) return next(err);
+        // otherwise, store hashedPassword in DB
+        const user = new User({
           username: req.body.username,
-          password: req.body.password,
+          password: hashedPassword,
           avatar: req.body.avatar,
           status: 'basic',
-        });
-        user.save(function (err) {
+        }).save((err) => {
           if (err) {
             return next(err);
           }
-          // Successful - redirect to home page
+          //Successful - redirect to home page
           let msg = `you have signed up as ${req.body.username}`;
           res.render('index', { title: 'Members Only', msg, user });
         });
-      }
-    },
-  ];
+      });
+    }
+  },
+];
--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: { type: String, required: true, maxLength: 20 },
-    password: { type: String, required: true, maxLength: 20 },
+    username: { type: String, required: true, minLength: 3, maxLength: 15 },
+    password: { type: String, required: true, minLength: 6 },
     avatar: { type: String, required: true },
     status: { type: String, required: true },
 });
