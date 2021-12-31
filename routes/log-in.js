45e4c829a3b080a5c89ff6cd95fe29412df11c7d
chris <mr.chrisnash@gmail.com>
2021-12-31 12:45:32 -0800

    error message on authentication failure

--- routes/log-in.js
+++ routes/log-in.js
@@ -10,32 +10,21 @@ router.get('/', login_controller.login_get);
 // POST login page - not working!
 //router.post('/', login_controller.login_post);
 
-
-
 //passport looks at request body for username and password and runs LocalStrategy to search db
 //it then creates a session cookie we can access to see if a user is logged in
-// router.post(
-//   "/",
-//   passport.authenticate("local", {
-//     successRedirect: "/",
-//     failureRedirect: "/log-in"
-//   })
-// );
-
+// TODO - import this from loginController
 router.post('/', function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
     if (err) { return next(err); }
-    if (!user) { return res.redirect('/log-in'); }
-    
+    if (!user) { 
+      return res.render('log-in', { title: 'Log in to your MembersOnly account.', msg: 'Username or Password incorrect.' });
+    }
     //login user
     req.logIn(user, function(err) {
       if (err) { return next(err); }
       //success
-      //return res.redirect('/', { user });
       return res.render('index', { title: 'You have successfully logged in!', user });
-
     });
-
   })(req, res, next);
 });
 
