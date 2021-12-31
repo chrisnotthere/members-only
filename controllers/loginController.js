1b79db819fa6f89329b3d5f1e81ec66ad890f272
chris <mr.chrisnash@gmail.com>
2021-12-30 16:49:07 -0800

    trying to implement login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -13,30 +13,42 @@ exports.login_get = function (req, res, next) {
 
 // Display login form on GET.
 exports.login_post = function (req, res, next) {
-  res.render('log-in', { title: 'Log in to your MembersOnly account.' });
+  //res.render('log-in', { title: 'Log in to your MembersOnly account.' });
 
-  passport.use(
-    new LocalStrategy((username, password, done) => {
-      User.findOne({ username: username }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        }
-        bcrypt.compare(password, user.password, (err, res) => {
-          if (res) {
-            // passwords match! log user in
-            return done(null, user);
-          } else {
-            // passwords do not match!
-            //return done(null, false, { message: "Incorrect password" });
-            res.render('log-in', { title: 'Log in to your MembersOnly account.', msg: 'Incorrect password' });
+  // Validate and sanitize fields.
+  body("username")
+    .trim()
+    .isLength({ min: 3, max: 15 })
+    .escape()
+    .withMessage("Username must be between 3-15 characters long."),
+  body("password")
+    .trim()
+    .isLength({ min: 6 })
+    .escape()
+    .withMessage("Password must be at least 6 characters long."),
+
+    passport.use(
+      new LocalStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+          if (err) {
+            return done(err);
           }
+          if (!user) {
+            return done(null, false, { message: "Incorrect username" });
+          }
+          bcrypt.compare(password, user.password, (err, res) => {
+            if (res) {
+              // passwords match! log user in
+              return done(null, user);
+            } else {
+              // passwords do not match!
+              //return done(null, false, { message: "Incorrect password" });
+              res.render('log-in', { title: 'Log in to your MembersOnly account.', msg: 'Incorrect password' });
+            }
+          });
         });
-      });
-    })
-  );
+      })
+    );
 
   // save user.id in cookie/session
   passport.serializeUser(function (user, done) {
@@ -53,7 +65,7 @@ exports.login_post = function (req, res, next) {
   passport.authenticate("local", {
     successRedirect: "/index",
     failureRedirect: "/index",
-  })
+  });
 
 };
 
