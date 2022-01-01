e244f63dbc73fc5e5fdd9b235093b5968b474984
chris <mr.chrisnash@gmail.com>
2022-01-01 12:01:57 -0800

    moved log-in logic to controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,24 +1,29 @@
-var User = require("../models/user");
-const bcrypt = require("bcryptjs");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-
-const { body, validationResult } = require("express-validator");
 
 // Display login form on GET.
 exports.login_get = function (req, res, next) {
   res.render('log-in', { title: 'Log in to your MembersOnly account.' });
 };
 
-// // Display login form on GET. ***currently this does not get called...***
-// exports.login_post = function (req, res, next) {
-
-//   console.log('login POST');
-//   //res.render('log-in', { title: 'Log in to your MembersOnly account.', msg: `${req.body.username} ${req.body.password}` });
-
-//   passport.authenticate("local", {
-//     successRedirect: "/",
-//     failureRedirect: "/log-in",
-//   });
-
-// }
+//passport looks at request body for username and password and runs LocalStrategy (in /config/passportConfig) to search db
+//it then creates a session cookie we can access to see if a user is logged in
+exports.login_post = function(req, res, next) {
+  
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { 
+      //username/password combo not found
+      return res.render('log-in', { title: 'Log in to your MembersOnly account.', msg: 'Username or Password incorrect.' });
+    }
+    //login user
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      //success
+      console.log('---login success---');
+      //save the user to app.locals, this can be accessed from anywhere in the app
+      app.locals.currentUser = req.user;  
+      return res.redirect('/')
+      //return res.render('index', { title: 'logged in!' });
+    });
+  })(req, res, next);
+};
--- routes/log-in.js
+++ routes/log-in.js
@@ -1,36 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var login_controller = require('../controllers/loginController');
-const passport = require("passport");
 
 // GET login page
 router.get('/', login_controller.login_get);
 
 // POST login page - not working!
-//router.post('/', login_controller.login_post);
-
-//passport looks at request body for username and password and runs LocalStrategy to search db
-//it then creates a session cookie we can access to see if a user is logged in
-// TODO - import this from loginController
-router.post('/', function(req, res, next) {
-  
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { 
-      //username/password combo not found
-      return res.render('log-in', { title: 'Log in to your MembersOnly account.', msg: 'Username or Password incorrect.' });
-    }
-    //login user
-    req.logIn(user, function(err) {
-      if (err) { return next(err); }
-      //success
-      console.log('---login success---');
-      //save the user to app.locals, this can be accessed from anywhere in the app
-      app.locals.currentUser = req.user;  
-      return res.redirect('/')
-      //return res.render('index', { title: 'logged in!' });
-    });
-  })(req, res, next);
-});
+router.post('/', login_controller.login_post);
 
 module.exports = router;
