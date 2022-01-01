e5e8c790220a66f172b76b2c827cbf68d5c60c0e
chris <mr.chrisnash@gmail.com>
2022-01-01 13:18:01 -0800

    message schema

--- models/message.js
+++ models/message.js
@@ -3,8 +3,8 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var MessageSchema = new Schema({
-    title: { type: String, required: true, maxLength: 20 },
-    timestamp: { type: Date, required: true },
+    msgTitle: { type: String, required: true, maxLength: 20 },
+    timestamp: { type: String, required: true },
     text: { type: String, required: true, minLength: 1, maxLength: 100},
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
