ee75e4ede5c39e90322dbfe9479e6aebff0a8346
chris <mr.chrisnash@gmail.com>
2022-01-01 14:20:04 -0800

    display messages

---
+++ controllers/indexController.js
@@ -0,0 +1,19 @@
+var Message = require('../models/message');
+
+// Display list of all messages.
+exports.index_get = function (req, res, next) {
+  Message.find({})
+    .sort({ timestamp: 1 })
+    .populate("user")
+    .exec(function (err, list_messages) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("index", {
+        title: "Members Only",
+        message_list: list_messages,
+      });
+    });
+};
+
