01ced58c969dc3951131296df1ccfa0b54ac98a4
chris <mr.chrisnash@gmail.com>
2022-01-01 14:19:37 -0800

    display messages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var index_controller = require('../controllers/indexController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Members Only', user: req.user} );
-  res.render('index', { title: 'Members Only' } );
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   //res.render('index', { title: 'Members Only', user: req.user} );
+//   res.render('index', { title: 'Members Only' } );
+// });
+
+// GET home page
+router.get('/', index_controller.index_get);
 
 module.exports = router;
